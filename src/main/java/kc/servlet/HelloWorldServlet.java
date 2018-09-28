068e5d4d879657426e6aba32f2922df1f4ab00ff
Ezreal <1649811141@qq.com>
2018-09-28 23:23:04 +0800

    servlet
    1,抽象类 myservlet；
    2,2个具体实现；

---
+++ src/main/java/kc/servlet/HelloWorldServlet.java
@@ -0,0 +1,25 @@
+package kc.servlet;
+
+import kc.MyRequest;
+import kc.MyResponse;
+
+public class HelloWorldServlet extends MyServlet{
+
+	@Override
+	public void doGet(MyRequest myRequest, MyResponse myResponse) {
+		try {
+			myResponse.write("get world~~");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void doPost(MyRequest myRequest, MyResponse myResponse) {
+		try {
+			myResponse.write("post world~~~");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/kc/servlet/MyServlet.java
@@ -0,0 +1,20 @@
+package kc.servlet;
+
+import kc.MyRequest;
+import kc.MyResponse;
+
+public abstract class MyServlet {
+
+	public abstract void doGet(MyRequest myRequest, MyResponse myResponse);
+	
+	public abstract void doPost(MyRequest myRequest, MyResponse myResponse);
+	
+	public void service(MyRequest myRequest, MyResponse myResponse) {
+		
+		if (myRequest.getMethod().equalsIgnoreCase("POST")) {
+			doPost(myRequest, myResponse);
+		} else if (myRequest.getMethod().equalsIgnoreCase("GET")) {
+			doGet(myRequest, myResponse);
+		}
+	}
+}
