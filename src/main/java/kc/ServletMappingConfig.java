51b4b6758c62ea0b0d5b69bdc98415ac89396e6a
Ezreal <1649811141@qq.com>
2018-09-29 00:06:39 +0800

    servlet配置类

---
+++ src/main/java/kc/ServletMappingConfig.java
@@ -0,0 +1,14 @@
+package kc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServletMappingConfig {
+
+	public static List<ServletMapping> servletMappingList = new ArrayList<>();
+	
+	static {
+		servletMappingList.add(new ServletMapping("findGirl", "/girl", "kc.servlet.FindGirlServlet"));
+		servletMappingList.add(new ServletMapping("helloWorld", "/world", "kc.servlet.HelloWorldServlet"));
+	}
+}
